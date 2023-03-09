b9972c9321cf013a923357b801d522b2167829b6
evan <evansullivan74@gmail.com>
2023-03-09 20:40:12 +0000

    created a splash screen with the apps logo

---
+++ app/src/main/java/origin/wit/socialmediaart/activities/SplashScreen.kt
@@ -0,0 +1,20 @@
+package origin.wit.socialmediaart.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import origin.wit.socialmediaart.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen, Signup::class.java))
+            finish()
+        },3000)
+
+
+    }
+}
